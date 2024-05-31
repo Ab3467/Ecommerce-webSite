4b85abab864119ef28526573da92b538d249bcc0
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-31 18:48:37 +0500

    changes

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div className=' text-slate-800'>
+    <div className=' text-white bg-black w-full h-full p-2'>
       Ecommerce website
     </div>
   )
