ce67c5bb6e3aa43416479c4473a8a8166d628e29
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-31 20:23:08 +0500

    changes

--- ecommerce/src/components/Navbar/Navbar.js
+++ ecommerce/src/components/Navbar/Navbar.js
@@ -16,7 +16,7 @@ export default function Navbar() {
                 <div className=" group">
                    <input type="text" placeholder='search' 
                    className=' w-[200px] sm:w-[200px] group-hover:w-[300px] transition duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none
-                   focus:border-1 focus:border-orange-400'
+                   focus:border-1 focus:border-orange-400 focus:border-primary'
                    />
                 </div>
                </div>
--- ecommerce/tailwind.config.js
+++ ecommerce/tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+       primary: "#fea928",
+       secondary: "#ed8900"
+      },
+      container:{
+        center: true,
+        padding:{
+        DEFAULT: "1rem",
+        sm:"3rem"
+        },
+      }
+    },
   },
   plugins: [],
 }
