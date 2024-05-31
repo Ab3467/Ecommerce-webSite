d2b68952f04a970712ce19e60f0e3fe8a529cbc7
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-31 21:31:34 +0500

    changes

--- ecommerce/src/components/Navbar/Navbar.js
+++ ecommerce/src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../../assets/logo.png";
-import {IoMdSearch} from "react-icons/io"
+import { IoMdSearch } from "react-icons/io";
 
 export default function Navbar() {
   return (
@@ -15,14 +15,14 @@ export default function Navbar() {
                 Shop here
               </a>
             </div>
-            <div className="">
+            <div>
               <div className=" group">
                 <input
                   type="text"
                   placeholder="search"
                   className=" w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary"
                 />
-                <IoMdSearch/>
+                <IoMdSearch className=" flex" />
               </div>
             </div>
           </div>
