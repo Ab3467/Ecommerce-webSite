386c0eef4edcb4c70bd9fb0310267324de50520a
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-05-31 20:24:37 +0500

    changes

--- ecommerce/src/components/Navbar/Navbar.js
+++ ecommerce/src/components/Navbar/Navbar.js
@@ -17,7 +17,7 @@ export default function Navbar() {
                 <div className=" group">
                    <input type="text" placeholder='search' 
                    className=' w-[200px] sm:w-[200px] group-hover:w-[300px] transition duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none
-                   focus:border-1 focus:border-orange-400 focus:border-primary'
+                   focus:border-1 focus:border-primary'
                    />
                 </div>
                </div>
