500b5a83ef98f7de04c9a2d6a297d80362cbbc66
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-01 19:58:51 +0500

    Chnages

--- ecommerce/src/components/Navbar/Navbar.js
+++ ecommerce/src/components/Navbar/Navbar.js
@@ -22,7 +22,7 @@ export default function Navbar() {
                   placeholder="search"
                   className=" w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary"
                 />
-                <IoMdSearch className="" />
+                <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
               </div>
             </div>
           </div>
@@ -37,3 +37,5 @@ export default function Navbar() {
     </div>
   );
 }
+
+// yeah its just for comment
