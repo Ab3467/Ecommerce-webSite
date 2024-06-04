335a83b5eaa41aad91cf227e8f5cb291e6d5709c
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-04 19:06:24 +0500

    Changes

--- ecommerce/src/components/Navbar/DarkMode.js
+++ ecommerce/src/components/Navbar/DarkMode.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import {light} from "../../assets/Website/light-mode-button.png"
 
 export default function DarkMode() {
   return (
     <div>
-      
+      <img src={light} alt="" />
     </div>
   )
 }
