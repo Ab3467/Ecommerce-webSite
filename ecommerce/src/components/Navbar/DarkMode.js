bef9911879e09c6eefcfceeeefe7d09976bcd306
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-06 18:35:59 +0500

    Changes

--- ecommerce/src/components/Navbar/DarkMode.js
+++ ecommerce/src/components/Navbar/DarkMode.js
@@ -4,8 +4,8 @@ import darkbtn from "../../assets/Website/dark-mode-button.png"
 
 export default function DarkMode() {
   return (
-    <div>
-      <img src={lightbtn} alt="" className=' w-12 cursor-pointer drop-shadow[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300'/>
+    <div className=' relative'>
+      <img src={lightbtn} alt="" className=' w-12 cursor-pointer drop-shadow[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10'/>
       <img src={darkbtn} alt="" className=' w-12 cursor-pointer drop-shadow[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300'/>
     </div>
   )
