e3150224f44173a036112055079d52f50a63dfce
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-09 14:57:59 +0500

    Changes

--- ecommerce/src/components/Navbar/Navbar.js
+++ ecommerce/src/components/Navbar/Navbar.js
@@ -72,7 +72,9 @@ export default function Navbar() {
                 <input
                   type="text"
                   placeholder="search"
-                  className=" w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary"
+                  className=" w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border
+                   border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary dark:border-gray-500
+                   dark:bg-gray-800"
                 />
                 <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
               </div>
@@ -97,7 +99,7 @@ export default function Navbar() {
       </div>
 
       {/*Lower Navbar*/}
-      <div className=" flex justify-center border-b shadow-sm">
+      <div className=" flex justify-center border-b shadow-md">
         <ul className=" sm:flex hidden items-center gap-4">
           {Menu.map((data) => (
             <li key={data.id}>
@@ -116,12 +118,11 @@ export default function Navbar() {
                 <FaCaretDown className=" transition-all duration-200 group-hover:rotate-180"></FaCaretDown>
               </span>
             </a>
-            <div className=" absolute z-[9999] hidden group-hover:block w-[150px] rounded-md bg-white p-2 text-black">
+            <div className=" absolute z-[9999] hidden group-hover:block w-[150px] rounded-md bg-white p-2 text-black shadow-md">
               <ul>
                 {Dropdownlinks.map((data) => (
                   <li key={data.id}>
-                    <a href={data.link}
-                    className=" inline-block w-full rounded-md p-2 hover:bg-primary/20"
+                    <a href={data.link} className=" inline-block w-full rounded-md p-2 hover:bg-primary/20"
                     >{data.name}</a>
                   </li>
                 ))}
