632b7672cca9fdf5aa0c4a76c39334e8014fda5d
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-09 16:02:01 +0500

    Changes

--- ecommerce/src/components/Hero/Hero.js
+++ ecommerce/src/components/Hero/Hero.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import sale from "../../assets/hero/sale.png"
+import shopping from "../../assets/hero/shopping.png"
+import women from "../../assets/hero/women.png"
 
 export default function Hero() {
   return (
