6f094795a5b64fc5a9ac9169a987dde175ace2c4
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-09 18:46:48 +0500

    Changes

--- ecommerce/src/components/Hero/Hero.js
+++ ecommerce/src/components/Hero/Hero.js
@@ -1,34 +1,38 @@
-import React from 'react'
-import sale from "../../assets/hero/sale.png"
-import shopping from "../../assets/hero/shopping.png"
-import women from "../../assets/hero/women.png"
-
+import React from "react";
+import sale from "../../assets/hero/sale.png";
+import shopping from "../../assets/hero/shopping.png";
+import women from "../../assets/hero/women.png";
 
 const Imagelist = [
-    {
-        id: 1,
-        img: women,
-        title: "",
-        description: ""
-    },
-    {
-        id: 2,
-        img: shopping,
-        title: "",
-        description: ""
-    },
-    {
-        id: 3,
-        img: sale,
-        title: "",
-        description: ""
-    },
-]
+  {
+    id: 1,
+    img: women,
+    title: "",
+    description: "",
+  },
+  {
+    id: 2,
+    img: shopping,
+    title: "",
+    description: "",
+  },
+  {
+    id: 3,
+    img: sale,
+    title: "",
+    description: "",
+  },
+];
 
 export default function Hero() {
   return (
-    <div>
-      Hero
-    </div>
-  )
+    <>
+      <div>
+        {/* Background pattern*/}
+
+        <div></div>
+        {/*hero section*/}
+      </div>
+    </>
+  );
 }
