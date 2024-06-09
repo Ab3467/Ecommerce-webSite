303c95a9b4f0ad25726928d52a9f61057c5e7ff8
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-09 13:19:28 +0500

    Changes

--- ecommerce/src/components/Navbar/DarkMode.js
+++ ecommerce/src/components/Navbar/DarkMode.js
@@ -26,7 +26,7 @@ export default function DarkMode() {
         src={lightbtn}
         alt=""
         className={
-          " w-12 cursor-pointer drop-shadow[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10"
+          ` w-12 cursor-pointer drop-shadow[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute right-0 z-10 ${theme === "dark" ? "opacity-0" : " opacity-100"}`
         }
       />
       <img
