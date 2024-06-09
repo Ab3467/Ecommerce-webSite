26eb1d3030ed744446c1c9b7e2d2ce57e05819cc
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-09 16:25:45 +0500

    Changes

--- ecommerce/src/components/Hero/Hero.js
+++ ecommerce/src/components/Hero/Hero.js
@@ -3,10 +3,32 @@ import sale from "../../assets/hero/sale.png"
 import shopping from "../../assets/hero/shopping.png"
 import women from "../../assets/hero/women.png"
 
+
+const Imagelist = [
+    {
+        id: 1,
+        img: women,
+        title: "",
+        description: ""
+    },
+    {
+        id: 2,
+        img: shopping,
+        title: "",
+        description: ""
+    },
+    {
+        id: 3,
+        img: sale,
+        title: "",
+        description: ""
+    },
+]
+
 export default function Hero() {
   return (
     <div>
-      
+      Hero
     </div>
   )
 }
