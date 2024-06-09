82ca98554729e4140632d835cbf9ade613f58162
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-09 15:10:04 +0500

    Changes

--- ecommerce/src/components/Navbar/Navbar.js
+++ ecommerce/src/components/Navbar/Navbar.js
@@ -55,26 +55,21 @@ export default function Navbar() {
   return (
     <div>
       {/*Upper Navbar*/}
-      <div className=" shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
+      <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
         <div className="bg-primary/40 py-2">
-          <div className=" container flex justify-between items-center">
+          <div className="container flex justify-between items-center">
             <div>
-              <a
-                href="#"
-                className=" font-bold text-2xl sm:text-3xl flex gap-2"
-              >
-                <img src={logo} alt="logo" className=" w-10" />
+              <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
+                <img src={logo} alt="logo" className="w-10" />
                 Shop here
               </a>
             </div>
-            <div className=" flex justify-between items-center gap-4">
+            <div className="flex justify-between items-center gap-4">
               <div className="relative group hidden sm:block">
                 <input
                   type="text"
                   placeholder="search"
-                  className=" w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border
-                   border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary dark:border-gray-500
-                   dark:bg-gray-800"
+                  className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary dark:border-gray-500 dark:bg-gray-800"
                 />
                 <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
               </div>
@@ -83,12 +78,12 @@ export default function Navbar() {
             {/* Order button */}
             <button
               onClick={() => alert("Ordering yet not available")}
-              className=" bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white py-1 px-4 rounded-full items-center flex gap-3 group"
+              className="bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white py-1 px-4 rounded-full items-center flex gap-3 group"
             >
-              <span className=" group-hover:block hidden translate-all duration-300">
+              <span className="group-hover:block hidden translate-all duration-300">
                 Order
               </span>
-              <FaCartShopping className=" text-xl text-white drop-shadow-sm cursor-pointer " />
+              <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
             </button>
             {/* DarkMode Button*/}
             <div>
@@ -99,31 +94,29 @@ export default function Navbar() {
       </div>
 
       {/*Lower Navbar*/}
-      <div className=" flex justify-center border-b shadow-md">
-        <ul className=" sm:flex hidden items-center gap-4">
+      <div className="flex justify-center border-b shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200">
+        <ul className="sm:flex hidden items-center gap-4">
           {Menu.map((data) => (
             <li key={data.id}>
-              <a
-                href={data.link}
-                className=" inline-block px-4 hover:text-primary duration-200"
-              >
+              <a href={data.link} className="inline-block px-4 hover:text-primary duration-200">
                 {data.name}
               </a>
             </li>
           ))}
-          <li className=" group relative cursor-pointer">
+          <li className="group relative cursor-pointer">
             <a href="#" className="flex items-start gap-[2px] py-2">
               Trending Products
               <span>
-                <FaCaretDown className=" transition-all duration-200 group-hover:rotate-180"></FaCaretDown>
+                <FaCaretDown className="transition-all duration-200 group-hover:rotate-180"></FaCaretDown>
               </span>
             </a>
-            <div className=" absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white p-2 text-black shadow-md">
+            <div className="absolute z-[9999] hidden group-hover:block w-[200px] rounded-md bg-white dark:bg-gray-900 p-2 text-black dark:text-white shadow-md">
               <ul>
                 {Dropdownlinks.map((data) => (
                   <li key={data.id}>
-                    <a href={data.link} className=" inline-block w-full rounded-md p-2 hover:bg-primary/20"
-                    >{data.name}</a>
+                    <a href={data.link} className="inline-block w-full rounded-md p-2 hover:bg-primary/20">
+                      {data.name}
+                    </a>
                   </li>
                 ))}
               </ul>
@@ -131,6 +124,6 @@ export default function Navbar() {
           </li>
         </ul>
       </div>
-    </div> 
+    </div>
   );
 }
