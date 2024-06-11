da9f3cd83c680b530b9abbb47894db521ac3e772
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-11 19:07:19 +0500

    Changes

--- ecommerce/src/components/Hero/Hero.js
+++ ecommerce/src/components/Hero/Hero.js
@@ -25,18 +25,18 @@ const Imagelist = [
   },
 ];
 
- var setting = {
+var setting = {
   dots: false,
   arrow: false,
   infinite: true,
-  speed : 800,
+  speed: 800,
   slidesToScroll: 1,
-  autoPlay : true,
-  autoPlaySpeed : 4000,
+  autoPlay: true,
+  autoPlaySpeed: 4000,
   cssEase: "ease-in-out",
   pauseOnHover: false,
-  pauseOnFocus: true
- }
+  pauseOnFocus: true,
+};
 
 export default function Hero() {
   return (
@@ -46,39 +46,38 @@ export default function Hero() {
         <div className=" h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-9"></div>
         {/*hero section*/}
         <div className=" container pb-8 sm:pb-0 ">
-
-          <Slider>
-          <div className="">
-            <div className=" grid grid-cols-1 sm:grid-cols-2">
-              <div className=" flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
-                <h1 className=" text-5xl sm:text-6xl lg:text-7xl font-bold">
-                  Upto 50% off for all Men's wear
-                </h1>
-                <p className=" text-sm">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Delectus quidem ullam dolorem, velit est quis, dicta sint iure
-                  nobis enim corporis!
-
-                </p>
-                <div className="">
-                  <button className=" bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
-                    Order now
-                  </button>
+          <Slider {...setting}>
+            {Imagelist.map((data) => (
+              <div className="">
+                <div className=" grid grid-cols-1 sm:grid-cols-2">
+                  <div className=" flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
+                    <h1 className=" text-5xl sm:text-6xl lg:text-7xl font-bold">
+                      Upto 50% off for all Men's wear
+                    </h1>
+                    <p className=" text-sm">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Delectus quidem ullam dolorem, velit est quis, dicta sint
+                      iure nobis enim corporis!
+                    </p>
+                    <div className="">
+                      <button className=" bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
+                        Order now
+                      </button>
+                    </div>
+                  </div>
+                  <div className=" order-2 sm:order-1">
+                    <div className=" relative z-10">
+                      <img
+                        src={women}
+                        alt=""
+                        className=" w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 object-contain mx-auto"
+                      />
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className=" order-2 sm:order-1">
-                <div className=" relative z-10">
-                  <img
-                    src={women}
-                    alt=""
-                    className=" w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 object-contain mx-auto"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
+            ))}
           </Slider>
-          
         </div>
       </div>
     </>
