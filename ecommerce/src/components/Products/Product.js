9488110b8868a7d4940ef8c83833c7264fc8451f
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-11 19:54:42 +0500

    Changes

--- ecommerce/src/components/Products/Product.js
+++ ecommerce/src/components/Products/Product.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function Product() {
   return (
-    <div>
-      
+    <div className=" justify-center items-center mt-10 p-2 flex text-primary">
+      <h4>Top selling Products For You</h4>
+      <div className=" flex justify-center items-center">
+        <h1>Products</h1>
+      </div>
     </div>
-  )
+  );
 }
