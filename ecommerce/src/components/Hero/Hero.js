afc559d7f04deb47d03d01c71949e441c7244311
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-11 18:09:23 +0500

    Changes

--- ecommerce/src/components/Hero/Hero.js
+++ ecommerce/src/components/Hero/Hero.js
@@ -30,8 +30,23 @@ export default function Hero() {
       <div className=" relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200">
         {/* Background pattern*/}
 
-        <div></div>
+        <div className=" h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-9">
+
+        </div>
         {/*hero section*/}
+        <div className=" container pb-8 sm:pb-0">
+           <div className="">
+            <div className=" grid grid-cols-1 sm:grid-cols-2">
+              <div className="">
+                <h1></h1>
+                <p></p>
+                <div className="">
+                  <button>Order now</button>
+                </div>
+              </div>
+            </div>
+           </div>
+        </div>
       </div>
     </>
   );
