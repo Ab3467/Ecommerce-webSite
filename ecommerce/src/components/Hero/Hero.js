b3966d9af3f565dd31d145f6bf7910663b2b7d32
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-11 19:34:13 +0500

    Changes

--- ecommerce/src/components/Hero/Hero.js
+++ ecommerce/src/components/Hero/Hero.js
@@ -3,38 +3,39 @@ import sale from "../../assets/hero/sale.png";
 import shopping from "../../assets/hero/shopping.png";
 import women from "../../assets/hero/women.png";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 const Imagelist = [
   {
     id: 1,
     img: women,
     title: "30% off on all Women's Wear",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit Delectus quidem ullam dolorem, velit est quis, dicta sint iure nobis enim corporis"
+    description:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit Delectus quidem ullam dolorem, velit est quis, dicta sintiure nobis enim corporis",
   },
   {
     id: 2,
     img: shopping,
     title: "Upto 50% off for all Men's Wear",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit Delectus quidem ullam dolorem, velit est quis, dicta sint iure nobis enim corporis",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Delectus quidem ullam dolorem, velit est quis, dicta sintiure nobis enim corporis",
   },
   {
     id: 3,
     img: sale,
     title: "70% off on all Products Sale",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit Delectus quidem ullam dolorem, velit est quis, dicta sint iure nobis enim corporis",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit Delectus quidem ullam dolorem, velit est quis, dicta sintiure nobis enim corporis",
   },
 ];
 
-var settings = {
+var setting = {
   dots: false,
-  arrows: false,
+  arrows: false, 
   infinite: true,
   speed: 800,
   slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 4000,
+  autoplay: true, 
+  autoplaySpeed: 4000, 
   cssEase: "ease-in-out",
   pauseOnHover: false,
   pauseOnFocus: true,
@@ -42,32 +43,42 @@ var settings = {
 
 export default function Hero() {
   return (
-    <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200">
-      {/* Background pattern */}
-      <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-9"></div>
-      {/* Hero section */}
-      <div className="container pb-8 sm:pb-0">
-        <Slider {...settings}>
-          {Imagelist.map((data) => (
-            <div key={data.id}> {/* Added key prop */}
-              <div className="grid grid-cols-1 sm:grid-cols-2">
-                <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
-                  <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">{data.title}</h1>
-                  <p className="text-sm">{data.description}</p>
-                  <div className="">
-                    <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">Order now</button>
+    <>
+      <div className=" relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200">
+        {/* Background pattern*/}
+        <div className=" h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-9"></div>
+        {/*hero section*/}
+        <div className=" container pb-8 sm:pb-0 ">
+          <Slider {...setting}>
+            {Imagelist.map((data) => (
+              <div className="">
+                <div className=" grid grid-cols-1 sm:grid-cols-2">
+                  <div className=" flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
+                    <h1 className=" text-5xl sm:text-6xl lg:text-7xl font-bold">
+                      {data.title}
+                    </h1>
+                    <p className=" text-sm">{data.description}</p>
+                    <div className="">
+                      <button className=" bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
+                        Order now
+                      </button>
+                    </div>
                   </div>
-                </div>
-                <div className="order-2 sm:order-1">
-                  <div className="relative z-10">
-                    <img src={data.img} alt="" className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 lg:scale-125 object-contain mx-auto" />
+                  <div className=" order-2 sm:order-1">
+                    <div className=" relative z-10">
+                      <img
+                        src={data.img}
+                        alt=""
+                        className=" w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 lg:scale-125 object-contain mx-auto"
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </Slider>
+            ))}
+          </Slider>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
