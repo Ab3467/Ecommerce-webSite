6d8368ff3f04bc5cac39bc29c1844f261803001d
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-11 21:45:11 +0500

    Changes

--- ecommerce/src/components/Products/Product.js
+++ ecommerce/src/components/Products/Product.js
@@ -49,34 +49,50 @@ export default function Product() {
     },
   ];
   return (
-    <div className=" mt-14 mb-12">
-      <div className=" container">
-        <div className=" text-center mb-10 max-w-[600px] mx-auto">
-          <p data-aos = "fade-up" className="text-sm text-primary">Top selling Products For You</p>
-          <h1 data-aos = "fade-up" className="text-3xl font-bold">Products</h1>
-          <p data-aos = "fade-up" className=" text-xs text-gray-400">
+    <div className="mt-14 mb-12 dark:bg-gray-900">
+      <div className="container">
+        <div className="text-center mb-10 max-w-[600px] mx-auto">
+          <p
+            data-aos="fade-up"
+            className="text-sm text-primary dark:text-white"
+          >
+            Top selling Products For You
+          </p>
+          <h1
+            data-aos="fade-up"
+            className="text-3xl font-bold dark:text-white"
+          >
+            Products
+          </h1>
+          <p
+            data-aos="fade-up"
+            className="text-xs text-gray-400 dark:text-gray-300"
+          >
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
             magnam eos dicta ullam
           </p>
         </div>
 
-        <div className=" grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-5">
+        <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-5">
           {Productsdata.map((data) => (
-            <div 
-            data-aos = "fade-up"
-            data-aos-delay = {data.aosDelay}
-            key={data.id} 
-            className=" space-y-3">
+            <div
+              data-aos="fade-up"
+              data-aos-delay={data.aosDelay}
+              key={data.id}
+              className="space-y-3 dark:text-white"
+            >
               <img
                 src={data.img}
                 alt=""
-                className=" h-[220px] w-[150px] object-cover rounded-md"
+                className="h-[220px] w-[150px] object-cover rounded-md"
               />
               <div>
-                <h3 className=" font-semibold">{data.title}</h3>
-                <p className=" text-sm text-gray-600">{data.color}</p>
-                <div className=" flex items-center gap-1">
-                  <FaStar className=" text-yellow-400" />
+                <h3 className="font-semibold">{data.title}</h3>
+                <p className="text-sm text-gray-600 dark:text-gray-300">
+                  {data.color}
+                </p>
+                <div className="flex items-center gap-1">
+                  <FaStar className="text-yellow-400" />
                   <span>{data.Rating}</span>
                 </div>
               </div>
