880d9755d0e4abc0236c50030ee9d6a87d2c2e6b
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-11 19:01:54 +0500

    Changes

--- ecommerce/src/index.js
+++ ecommerce/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick"
+import "slick-carousel/slick/slick-theme.css"
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
