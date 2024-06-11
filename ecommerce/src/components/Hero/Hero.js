4dcbff453a132b9627d327f39a5b5ae925cee9a4
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-11 21:27:46 +0500

    Changes

--- ecommerce/src/components/Hero/Hero.js
+++ ecommerce/src/components/Hero/Hero.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import sale from "../../assets/hero/sale.png";
 import shopping from "../../assets/hero/shopping.png";
 import women from "../../assets/hero/women.png";
 import Slider from "react-slick";
+import AOS from "aos";
+import "aos/dist/aos.css"; // Import AOS styles
 
 const Imagelist = [
   {
@@ -42,51 +44,56 @@ var setting = {
 };
 
 export default function Hero() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      once: true, // Whether animation should happen only once
+    });
+  }, []);
+
   return (
     <>
-      <div className=" relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200">
-        {/* Background pattern*/}
-        <div className=" h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-9"></div>
-        {/*hero section*/}
-        <div className=" container pb-8 sm:pb-0 ">
+      <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200">
+        {/* Background pattern */}
+        <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-9"></div>
+        {/* Hero section */}
+        <div className="container pb-8 sm:pb-0">
           <Slider {...setting}>
             {Imagelist.map((data) => (
-              <div>
-                <div className=" grid grid-cols-1 sm:grid-cols-2">
-                  <div className=" flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
+              <div key={data.id}>
+                <div className="grid grid-cols-1 sm:grid-cols-2">
+                  <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
                     
-                    <h1 
-                    className=" text-5xl sm:text-6xl lg:text-7xl font-bold"
-                    data-aos = "zoom-out"
-                    data-aos-duration = "500"
-                    data-aos-once =" true ">
-                    {data.title}
+                    <h1
+                      className="text-5xl sm:text-6xl lg:text-7xl font-bold"
+                      data-aos="zoom-out"
+                    >
+                      {data.title}
                     </h1>
-                   
-                    <p 
-                    className=" text-sm"
-                    data-aos = "fade-up"
-                    data-aos-duration = "500"
-                    data-aos-delay ="100">
-                    {data.description}
+                    
+                    <p
+                      className="text-sm"
+                      data-aos="fade-up"
+                      data-aos-delay="100"
+                    >
+                      {data.description}
                     </p>
-
-                    <div 
-                     data-aos = "zoom-out"
-                     data-aos-duration = "500"
-                     data-aos-delay ="300">
-
-                      <button className=" bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
+                    
+                    <div
+                      data-aos="zoom-out"
+                      data-aos-delay="300"
+                    >
+                      <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
                         Order now
                       </button>
                     </div>
                   </div>
-                  <div className=" order-2 sm:order-1">
-                    <div className=" relative z-10">
+                  <div className="order-2 sm:order-1">
+                    <div className="relative z-10">
                       <img
                         src={data.img}
-                        alt=""
-                        className=" w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 lg:scale-125 object-contain mx-auto"
+                        alt={data.title}
+                        className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 lg:scale-125 object-contain mx-auto"
                       />
                     </div>
                   </div>
