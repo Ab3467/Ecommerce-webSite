ec3f49e8240a856dcdeb15d5033566b80329e37a
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-11 20:06:18 +0500

    Changes

--- ecommerce/src/components/Navbar/Navbar.js
+++ ecommerce/src/components/Navbar/Navbar.js
@@ -54,7 +54,7 @@ const Dropdownlinks = [
 export default function Navbar() {
   return (
     <div>
-      {/*Upper Navbar*/}
+      {/* Upper Navbar */}
       <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
         <div className="bg-primary/40 py-2">
           <div className="container flex justify-between items-center">
@@ -64,7 +64,7 @@ export default function Navbar() {
                 Shop here
               </a>
             </div>
-            <div className="flex justify-between items-center gap-4">
+            <div className="flex items-center gap-4">
               <div className="relative group hidden sm:block">
                 <input
                   type="text"
@@ -73,27 +73,22 @@ export default function Navbar() {
                 />
                 <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
               </div>
-            </div>
-
-            {/* Order button */}
-            <button
-              onClick={() => alert("Ordering yet not available")}
-              className="bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white py-1 px-4 rounded-full items-center flex gap-3 group"
-            >
-              <span className="group-hover:block hidden translate-all duration-300">
-                Order
-              </span>
-              <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
-            </button>
-            {/* DarkMode Button*/}
-            <div>
+              <button
+                onClick={() => alert("Ordering yet not available")}
+                className="bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white py-1 px-4 rounded-full items-center flex gap-3 group"
+              >
+                <span className="group-hover:block hidden translate-all duration-300">
+                  Order
+                </span>
+                <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
+              </button>
               <DarkMode />
             </div>
           </div>
         </div>
       </div>
 
-      {/*Lower Navbar*/}
+      {/* Lower Navbar */}
       <div className="flex justify-center border-b shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200">
         <ul className="sm:flex hidden items-center gap-4">
           {Menu.map((data) => (
