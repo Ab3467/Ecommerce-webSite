fa8ce935581f7922b7c550a58f938189b2988e1a
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-11 21:14:32 +0500

    Changes

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Product from './components/Products/Product'
 import AOS from "aos"
 
 export default function App() {
+  useEffect(()=>{
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    })
+    AOS.refresh();
+  },[])
   return (
     <div>
       <Navbar/>
