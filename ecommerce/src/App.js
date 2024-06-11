0f72b1b74861bd80063a8cfc5e046e6c134a9ee1
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-11 19:38:45 +0500

    Changes

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Product from './components/Products/Product'
 
 export default function App() {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Product/>
     </div>
   )
 }
