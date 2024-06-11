b1d2177ad1af985d52719ec2a14d06b8fd3c80a8
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-11 20:31:23 +0500

    Changes

--- ecommerce/src/components/Products/Product.js
+++ ecommerce/src/components/Products/Product.js
@@ -1,53 +1,53 @@
 import React from "react";
-import img1 from "../../assets/women/women.png"
-import img2 from "../../assets/women/women2.png"
-import img3 from "../../assets/women/women3.png"
-import img4 from "../../assets/women/women4.png"
+import img1 from "../../assets/women/women.png";
+import img2 from "../../assets/women/women2.png";
+import img3 from "../../assets/women/women3.png";
+import img4 from "../../assets/women/women4.png";
+import { FaStar } from "react-icons/fa6";
 
 export default function Product() {
-
-const Productsdata = [
+  const Productsdata = [
     {
-        id: 1,
-        img: img1,
-        title: "Women Ethnic",
-        Rating: 5,
-        color: "white",
-        aosDelay: "0",
+      id: 1,
+      img: img1,
+      title: "Women Ethnic",
+      Rating: 5,
+      color: "white",
+      aosDelay: "0",
     },
     {
-        id: 2,
-        img: img2,
-        title: "Women western",
-        Rating: 4.5,
-        color: "red",
-        aosDelay: "200",
+      id: 2,
+      img: img2,
+      title: "Women western",
+      Rating: 4.5,
+      color: "red",
+      aosDelay: "200",
     },
     {
-        id: 3,
-        img: img3,
-        title: "Goggles",
-        Rating: 4.7,
-        color: "brown",
-        aosDelay: "400",
+      id: 3,
+      img: img3,
+      title: "Goggles",
+      Rating: 4.7,
+      color: "brown",
+      aosDelay: "400",
     },
     {
-        id: 4,
-        img: img4,
-        title: "Printed T-Shirt",
-        Rating: 4.4,
-        color: "yellow",
-        aosDelay: "600",
+      id: 4,
+      img: img4,
+      title: "Printed T-Shirt",
+      Rating: 4.4,
+      color: "yellow",
+      aosDelay: "600",
     },
     {
-        id: 5,
-        img: img2,
-        title: "Fashion T-Shirt",
-        Rating: 4.5,
-        color: "pink",
-        aosDelay: "800",
-    }
-]
+      id: 5,
+      img: img2,
+      title: "Fashion T-Shirt",
+      Rating: 4.5,
+      color: "pink",
+      aosDelay: "800",
+    },
+  ];
   return (
     <div className=" mt-14 mb-12">
       <div className=" container">
@@ -61,18 +61,17 @@ const Productsdata = [
         </div>
 
         <div className="">
-            <div className="">
-                <img src="" alt="" 
-                className=" grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-5"/>
+          <div className="">
+            <img
+              src=""
+              alt=""
+              className=" grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-5"
+            />
 
-                {
-                    Productsdata.map((data)=>(
-                        <div>
-                            
-                        </div>
-                    ))
-                }
-            </div>
+            {Productsdata.map((data) => (
+              <div></div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
