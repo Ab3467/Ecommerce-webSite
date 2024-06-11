2632d8b6837e843248bea6f7ac4b9b0501951f04
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-11 20:11:07 +0500

    Changes

--- ecommerce/src/components/Products/Product.js
+++ ecommerce/src/components/Products/Product.js
@@ -2,13 +2,17 @@ import React from "react";
 
 export default function Product() {
   return (
-    <div className="mt-10 p-2 flex flex-col items-center">
-      <h4 className="text-sm text-primary">Top selling Products For You</h4>
-      <h1 className="text-3xl font-bold text-black">Products</h1>
-      <p className=" text-sm text-gray-500">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, magnam
-        eos dicta ullam 
-        </p>
+    <div className=" mt-14 mb-12">
+      <div className=" container">
+        <div className=" text-center mb-10 max-w-[600px] mx-auto">
+          <p className="text-sm text-primary">Top selling Products For You</p>
+          <h1 className="text-3xl font-bold text-black">Products</h1>
+          <p className=" text-sm text-gray-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+            magnam eos dicta ullam
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
