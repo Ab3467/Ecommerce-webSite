f77983910bfec04eeccc94a3f13b4cadb07ff184
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-11 20:11:46 +0500

    Changes

--- ecommerce/src/components/Products/Product.js
+++ ecommerce/src/components/Products/Product.js
@@ -6,7 +6,7 @@ export default function Product() {
       <div className=" container">
         <div className=" text-center mb-10 max-w-[600px] mx-auto">
           <p className="text-sm text-primary">Top selling Products For You</p>
-          <h1 className="text-3xl font-bold text-black">Products</h1>
+          <h1 className="text-3xl font-bold">Products</h1>
           <p className=" text-sm text-gray-500">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
             magnam eos dicta ullam
