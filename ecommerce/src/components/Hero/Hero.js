b4badb47fe0d24777a086fc59aad198862749e10
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-11 21:26:30 +0500

    Changes

--- ecommerce/src/components/Hero/Hero.js
+++ ecommerce/src/components/Hero/Hero.js
@@ -54,6 +54,7 @@ export default function Hero() {
               <div>
                 <div className=" grid grid-cols-1 sm:grid-cols-2">
                   <div className=" flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
+                    
                     <h1 
                     className=" text-5xl sm:text-6xl lg:text-7xl font-bold"
                     data-aos = "zoom-out"
@@ -61,8 +62,20 @@ export default function Hero() {
                     data-aos-once =" true ">
                     {data.title}
                     </h1>
-                    <p className=" text-sm">{data.description}</p>
-                    <div className="">
+                   
+                    <p 
+                    className=" text-sm"
+                    data-aos = "fade-up"
+                    data-aos-duration = "500"
+                    data-aos-delay ="100">
+                    {data.description}
+                    </p>
+
+                    <div 
+                     data-aos = "zoom-out"
+                     data-aos-duration = "500"
+                     data-aos-delay ="300">
+
                       <button className=" bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
                         Order now
                       </button>
