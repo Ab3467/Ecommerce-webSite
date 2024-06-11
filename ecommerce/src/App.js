d93ce548f673f8d5e76af3343a60dab10382969a
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-11 20:47:40 +0500

    Changes

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Product from './components/Products/Product'
+import AOS from "aos"
 
 export default function App() {
   return (
--- ecommerce/src/components/Products/Product.js
+++ ecommerce/src/components/Products/Product.js
@@ -63,13 +63,14 @@ export default function Product() {
         <div className=" grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-5">
 
             {Productsdata.map((data) => (
-              <div>
+              <div key={data.id} className=" space-y-3">
                 <img src={data.img} alt="" className=" h-[220px] w-[150px] object-cover rounded-md"/>
                 <div>
                     <h3 className=" font-semibold">{data.title}</h3>
                     <p className=" text-sm text-gray-600">{data.color}</p>
-                    <div>
+                    <div className=" flex items-center gap-1">
                         <FaStar className=" text-yellow-400"/>
+                        <span>{data.Rating}</span>
                     </div>
                 </div>
               </div>
