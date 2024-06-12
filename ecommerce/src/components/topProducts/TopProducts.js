015f9a5f80427ccea054cac703de4ca990461699
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-12 21:01:59 +0500

    changes

--- ecommerce/src/components/topProducts/TopProducts.js
+++ ecommerce/src/components/topProducts/TopProducts.js
@@ -4,6 +4,7 @@ import React from 'react';
 export default function TopProducts() {
   return (
     <div>
+
       <div className="container">
         <div className="text-left mb-10">
           <p data-aos="fade-up" className="text-sm text-primary">Top Rated Products For You</p>
@@ -12,6 +13,12 @@ export default function TopProducts() {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
             magnam eos dicta ullam
           </p>
+        </div>
+
+        <div className="">
+        {ProductDetails.map((data)=>(
+            <div></div>
+        ))}
         </div>
       </div>
     </div>
