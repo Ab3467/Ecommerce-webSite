b5c706d477e17392895e6de1e85d9484cc50a8e4
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-12 20:59:38 +0500

    changes

--- ecommerce/src/components/topProducts/TopProducts.js
+++ ecommerce/src/components/topProducts/TopProducts.js
@@ -1,18 +1,19 @@
-import React from 'react'
+// TopProducts.js
+import React from 'react';
 
 export default function TopProducts() {
   return (
     <div>
-        <div className="container">
-            <div className=" text-left mb-10">
-            <p data-aos = "fade-up" className="text-sm text-primary">Top Rated Products For You</p>
-            <h1 data-aos = "fade-up" className="text-3xl font-bold">Best Products</h1>
-            <p data-aos = "fade-up" className=" text-xs text-gray-400">
+      <div className="container">
+        <div className="text-left mb-10">
+          <p data-aos="fade-up" className="text-sm text-primary">Top Rated Products For You</p>
+          <h1 data-aos="fade-up" className="text-3xl font-bold">Best Products</h1>
+          <p data-aos="fade-up" className="text-xs text-gray-400">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
             magnam eos dicta ullam
           </p>
-            </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
