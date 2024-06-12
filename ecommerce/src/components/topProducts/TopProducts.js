7acb100d79882a596a89e1d9988da9a449f4687d
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-12 18:58:59 +0500

    changes

--- ecommerce/src/components/topProducts/TopProducts.js
+++ ecommerce/src/components/topProducts/TopProducts.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 export default function TopProducts() {
   return (
-    <div className="">
-        <div className="">
-            <p>Top Rated Products for you</p>
-            <h1>Best Products</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit asperiores modi Sit asperiores modi</p>
+    <div>
+        <div className="container">
+            <div className="">
+                <p></p>
+                <h1></h1>
+                <p></p>
+            </div>
         </div>
     </div>
   )
