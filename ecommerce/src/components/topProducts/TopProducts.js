497a89e38740c861405b975e37d7678aa37a908d
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-12 19:01:09 +0500

    changes

--- ecommerce/src/components/topProducts/TopProducts.js
+++ ecommerce/src/components/topProducts/TopProducts.js
@@ -4,10 +4,13 @@ export default function TopProducts() {
   return (
     <div>
         <div className="container">
-            <div className="">
-                <p></p>
-                <h1></h1>
-                <p></p>
+            <div className=" text-left mb-10">
+            <p data-aos = "fade-up" className="text-sm text-primary">Top Rated Products For You</p>
+            <h1 data-aos = "fade-up" className="text-3xl font-bold">Best Products</h1>
+            <p data-aos = "fade-up" className=" text-xs text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+            magnam eos dicta ullam
+          </p>
             </div>
         </div>
     </div>
