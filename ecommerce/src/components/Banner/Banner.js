8ae31d86d10ce799db090f3cc95fdbf83f8e48c9
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-13 18:05:16 +0500

    Changes:

--- ecommerce/src/components/Banner/Banner.js
+++ ecommerce/src/components/Banner/Banner.js
@@ -4,7 +4,7 @@ import img from "../../assets/women/women2.jpg"
 export default function Banner() {
   return (
     <div>
-      <div className="">
+      <div className=" min-h-[550px] flex justify-center items-center py-12 sm:py-0">
         <div className="">
             <div className="">
                 <img src={img} alt="" />
