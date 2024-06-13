2dcad8eede0b41f2cdf6767031d5c7b64022d38b
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-13 18:21:26 +0500

    Changes:

--- ecommerce/src/components/Banner/Banner.js
+++ ecommerce/src/components/Banner/Banner.js
@@ -1,24 +1,32 @@
-import React from 'react'
-import img from "../../assets/women/women2.jpg"
-import {GrSecure} from "react-icons/gr"
-import {IoFastFood} from "react-icons/io"
-import {GiFoodTruck} from "react-icons/gi"
+import React from "react";
+import img from "../../assets/women/women2.jpg";
+import { GrSecure } from "react-icons/gr";
+import { IoFastFood } from "react-icons/io";
+import { GiFoodTruck } from "react-icons/gi";
 
 export default function Banner() {
   return (
-      <div className=" min-h-[550px] flex justify-center items-center py-12 sm:py-0">
-        <div className=" container">
-            <div className=" grid grid-cols-1 sm:grid-cols-2 gap-6 items-center">
-            <div data-aos= "zoom-in">
-                <img src={img} alt="" className=' max-w-[400px] h-[350px] w-full mx-auto drop-shadow-[-10px_10px_12px_rgba(0,0,0,1)] object-cover'/>
-            </div>
-            <div className="">
-                <h1 className=' text-3xl sm:text-4xl font-bold'>Winter Sale upto 50% Off</h1>
-                <p className=' text-sm text-gray-500'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio</p>
-
-            </div>
+    <div className=" min-h-[550px] flex justify-center items-center py-12 sm:py-0">
+      <div className=" container">
+        <div className=" grid grid-cols-1 sm:grid-cols-2 gap-6 items-center">
+          <div data-aos="zoom-in">
+            <img
+              src={img}
+              alt=""
+              className=" max-w-[400px] h-[350px] w-full mx-auto drop-shadow-[-10px_10px_12px_rgba(0,0,0,1)] object-cover"
+            />
+          </div>
+          <div className="">
+            <h1 className=" text-3xl sm:text-4xl font-bold">
+              Winter Sale upto 50% Off
+            </h1>
+            <p className=" text-sm text-gray-500">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
+              reiciendis inventore iste ratione ex alias quis magni at optio
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
