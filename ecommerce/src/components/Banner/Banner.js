394263d83b6fb9b52ce12d11946c30ff330e222a
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-13 18:14:15 +0500

    Changes:

--- ecommerce/src/components/Banner/Banner.js
+++ ecommerce/src/components/Banner/Banner.js
@@ -10,7 +10,7 @@ export default function Banner() {
         <div className=" container">
             <div className=" grid grid-cols-1 sm:grid-cols-2 gap-6 items-center">
             <div data-aos= "zoom-in">
-                <img src={img} alt="" />
+                <img src={img} alt="" className=' max-w-[400px] h-[350px] w-full mx-auto drop-shadow-[-10px_10px_12px_rgba(0,0,0,1)] object-cover'/>
             </div>
         </div>
       </div>
