f3f43bf968e1d69b89a99d3b51a9b39010b6c6e0
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-13 18:20:45 +0500

    Changes:

--- ecommerce/src/components/Banner/Banner.js
+++ ecommerce/src/components/Banner/Banner.js
@@ -12,6 +12,11 @@ export default function Banner() {
             <div data-aos= "zoom-in">
                 <img src={img} alt="" className=' max-w-[400px] h-[350px] w-full mx-auto drop-shadow-[-10px_10px_12px_rgba(0,0,0,1)] object-cover'/>
             </div>
+            <div className="">
+                <h1 className=' text-3xl sm:text-4xl font-bold'>Winter Sale upto 50% Off</h1>
+                <p className=' text-sm text-gray-500'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio</p>
+
+            </div>
         </div>
       </div>
     </div>
