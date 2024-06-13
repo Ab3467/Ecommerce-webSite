6fad55243c40b8dd343cd6e978036911b7851042
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-13 17:55:34 +0500

    Changes:

--- ecommerce/src/components/topProducts/TopProducts.js
+++ ecommerce/src/components/topProducts/TopProducts.js
@@ -64,8 +64,10 @@ export default function TopProducts() {
                 </div>
                 <h1 className=" text-xl font-bold">{data.title}</h1>
                 <p className="text-gray-600 group-hover:text-white duration-300 text-sm line-clamp-2">{data.description}</p>
-                <button className=" bg-primary hover:scale-105 duration-300 text-white py-1 px-4 rounded-full mt-4 group-hover:bg-white group-hover:text-primary">
-                  Order Now
+                <button 
+                data-aos = "zoom-in"
+                className=" bg-primary hover:scale-105 duration-300 text-white py-1 px-4 rounded-full mt-4 group-hover:bg-white group-hover:text-primary">
+                 Order Now
                 </button>
               </div>
             </div>
