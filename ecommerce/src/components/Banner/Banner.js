3696b1caa1bf8cefea5586942a2597e2176ecd8a
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-13 18:06:29 +0500

    Changes:

--- ecommerce/src/components/Banner/Banner.js
+++ ecommerce/src/components/Banner/Banner.js
@@ -5,8 +5,8 @@ export default function Banner() {
   return (
     <div>
       <div className=" min-h-[550px] flex justify-center items-center py-12 sm:py-0">
-        <div className="">
-            <div className="">
+        <div className=" container">
+            <div className=" grid grid-cols-1 sm:grid-cols-2 gap-6 items-center">
                 <img src={img} alt="" />
             </div>
         </div>
