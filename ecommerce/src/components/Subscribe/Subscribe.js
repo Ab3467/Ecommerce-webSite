7f3817ea87e12cf323c1e8cbe6a06070dde56469
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-14 19:54:42 +0500

    Changes:

--- ecommerce/src/components/Subscribe/Subscribe.js
+++ ecommerce/src/components/Subscribe/Subscribe.js
@@ -4,7 +4,9 @@ import OrngPat from "../../assets/Website/orange-pattern.jpg"
 export default function Subscribe() {
   return (
     <div>
-      
+      <div className="">
+        
+      </div>
     </div>
   )
 }
