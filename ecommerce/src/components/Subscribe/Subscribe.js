f53372d2792c2d11c74768732aa4418e555c0e8c
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-14 20:04:44 +0500

    Changes:

--- ecommerce/src/components/Subscribe/Subscribe.js
+++ ecommerce/src/components/Subscribe/Subscribe.js
@@ -3,7 +3,12 @@ import Banner from "../../assets/Website/orange-pattern.jpg";
 
 export default function Subscribe() {
   const BannerImg = {
-    backgroundImage: `url(${Banner})`
+    backgroundImage: `url(${Banner})`,
+    backgroundPosition : "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    height: "100%",
+    width: "100%"
   };
 
   return (
