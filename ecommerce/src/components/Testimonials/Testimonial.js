147661aa945f979c561578a93f706abd5c8258f4
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-21 19:20:35 +0500

    Changes:

--- ecommerce/src/components/Testimonials/Testimonial.js
+++ ecommerce/src/components/Testimonials/Testimonial.js
@@ -28,7 +28,7 @@ const TestimonialData = [
   },
 ];
 
-export default function Testimonial() {
+const Testimonials = () => {
   var settings = {
     dots: true,
     arrows: false,
@@ -66,40 +66,59 @@ export default function Testimonial() {
       },
     ],
   };
+
   return (
     <div className="py-10 mb-10">
-      <div className=" container">
-        <div className=" text-center mb-10 max-w-[600px] mx-auto">
+      <div className="container">
+        {/* header section */}
+        <div className="text-center mb-10 max-w-[600px] mx-auto">
           <p data-aos="fade-up" className="text-sm text-primary">
             What our customers are saying
           </p>
           <h1 data-aos="fade-up" className="text-3xl font-bold">
             Testimonials
           </h1>
-          <p data-aos="fade-up" className=" text-xs text-gray-400">
+          <p data-aos="fade-up" className="text-xs text-gray-400">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit
             asperiores modi Sit asperiores modi
           </p>
+        </div>
 
-          {/* Testimonial Cards */}
-          <div>
-            <div>
-              <Slider {...settings}>
-                {TestimonialData.map((data) => (
-                  <div
-                    key={data.id}
-                    className=" flex flex-col gap-4 shadow-lg py-8 px-6 mx-4 rounded-full"
-                  >
-                    <div>
-                      <img src={data.img} alt="" />
+        {/* Testimonial cards */}
+        <div data-aos="zoom-in">
+          <Slider {...settings}>
+            {TestimonialData.map((data) => (
+              <div className="my-6">
+                <div
+                  key={data.id}
+                  className="flex flex-col gap-4 shadow-lg py-8 px-6 mx-4 rounded-xl dark:bg-gray-800 bg-primary/10 relative"
+                >
+                  <div className="mb-4">
+                    <img
+                      src={data.img}
+                      alt=""
+                      className="rounded-full w-20 h-20"
+                    />
+                  </div>
+                  <div className="flex flex-col items-center gap-4">
+                    <div className="space-y-3">
+                      <p className="text-xs text-gray-500">{data.text}</p>
+                      <h1 className="text-xl font-bold text-black/80 dark:text-light">
+                        {data.name}
+                      </h1>
                     </div>
                   </div>
-                ))}
-              </Slider>
-            </div>
-          </div>
+                  <p className="text-black/20 text-9xl font-serif absolute top-0 right-0">
+                    ,,
+                  </p>
+                </div>
+              </div>
+            ))}
+          </Slider>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Testimonials;
