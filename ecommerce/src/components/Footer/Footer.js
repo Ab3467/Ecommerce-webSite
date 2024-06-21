066f350fac31164e573b64713cca6365804343e2
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-21 20:08:46 +0500

    Changes:

--- ecommerce/src/components/Footer/Footer.js
+++ ecommerce/src/components/Footer/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import FooterLogo from "../../assets/logo.png"
+import Banner from "../../assets/Website/orange-pattern.jpg";
 
 export default function Footer() {
   return (
     <div>
       <div>
-        
+
       </div>
     </div>
   )
