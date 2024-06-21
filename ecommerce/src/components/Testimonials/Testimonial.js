db11a9455ed8fb67913fbe6da94137920d4c612f
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-21 19:16:13 +0500

    Changes:

--- ecommerce/src/components/Testimonials/Testimonial.js
+++ ecommerce/src/components/Testimonials/Testimonial.js
@@ -1,33 +1,32 @@
-import React from 'react'
+import React from "react";
 import Slider from "react-slick";
 
 const TestimonialData = [
-    {
-      id: 1,
-      name: "Victor",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
-      img: "https://picsum.photos/101/101",
-    },
-    {
-      id: 2,
-      name: "Satya Nadella",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
-      img: "https://picsum.photos/102/102",
-    },
-    {
-      id: 3,
-      name: "Virat Kohli",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
-      img: "https://picsum.photos/104/104",
-    },
-    {
-      id: 5,
-      name: "Sachin Tendulkar",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
-      img: "https://picsum.photos/103/103",
-    },
-  ];
-  
+  {
+    id: 1,
+    name: "Victor",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    img: "https://picsum.photos/101/101",
+  },
+  {
+    id: 2,
+    name: "Satya Nadella",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    img: "https://picsum.photos/102/102",
+  },
+  {
+    id: 3,
+    name: "Virat Kohli",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    img: "https://picsum.photos/104/104",
+  },
+  {
+    id: 5,
+    name: "Sachin Tendulkar",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+    img: "https://picsum.photos/103/103",
+  },
+];
 
 export default function Testimonial() {
   var settings = {
@@ -72,27 +71,32 @@ export default function Testimonial() {
       <div className=" container">
         <div className=" text-center mb-10 max-w-[600px] mx-auto">
           <p data-aos="fade-up" className="text-sm text-primary">
-          What our customers are saying
+            What our customers are saying
           </p>
           <h1 data-aos="fade-up" className="text-3xl font-bold">
             Testimonials
           </h1>
           <p data-aos="fade-up" className=" text-xs text-gray-400">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit asperiores modi Sit asperiores modi
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit
+            asperiores modi Sit asperiores modi
           </p>
 
           {/* Testimonial Cards */}
           <div>
             <div>
-           <Slider {...settings}>{
-            TestimonialData.map((data)=>(
-              <div></div>
-            ))
-            }</Slider>
+              <Slider {...settings}>
+                {TestimonialData.map((data) => (
+                  <div key={data.id} className=" flex flex-col gap-4 shadow-lg py-8 px-6 mx-4 rounded-full">
+                    <div>
+
+                    </div>
+                  </div>
+                ))}
+              </Slider>
             </div>
           </div>
         </div>
-        </div>
-    </div>  
-  )
+      </div>
+    </div>
+  );
 }
