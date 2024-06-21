6d0bffd8314d6ff099921a887fdb4db085e4b657
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-21 18:57:01 +0500

    Changes:

--- ecommerce/src/components/Testimonials/Testimonial.js
+++ ecommerce/src/components/Testimonials/Testimonial.js
@@ -6,14 +6,13 @@ export default function Testimonial() {
       <div className=" container">
         <div className=" text-center mb-10 max-w-[600px] mx-auto">
           <p data-aos="fade-up" className="text-sm text-primary">
-            Top selling Products For You
+          What our customers are saying
           </p>
           <h1 data-aos="fade-up" className="text-3xl font-bold">
-            Products
+            Testimonials
           </h1>
           <p data-aos="fade-up" className=" text-xs text-gray-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
-            magnam eos dicta ullam
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit asperiores modi Sit asperiores modi
           </p>
         </div>
         </div>
