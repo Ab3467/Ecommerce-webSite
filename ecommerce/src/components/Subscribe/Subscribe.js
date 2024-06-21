7106e8a29ed2bb7ac67addbead28de38636d1285
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-21 18:18:14 +0500

    Changes:

--- ecommerce/src/components/Subscribe/Subscribe.js
+++ ecommerce/src/components/Subscribe/Subscribe.js
@@ -18,7 +18,7 @@ export default function Subscribe() {
       style={BannerImg}
     >
       <div className=" container backdrop-blur-sm py-10">
-        <div className="">
+        <div className=" space-y-6 max-w-xl mx-auto">
           <h1 className=" text-2xl !text-center sm:text-left sm:text-4xl font-semibold">
             Get Notified about new Products{" "}
           </h1>
