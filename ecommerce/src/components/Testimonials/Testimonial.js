4899ea26555f17b0fd2bd2e4436e49df05725506
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-21 18:56:07 +0500

    Changes:

--- ecommerce/src/components/Testimonials/Testimonial.js
+++ ecommerce/src/components/Testimonials/Testimonial.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 export default function Testimonial() {
   return (
-    <div>
-      
-    </div>
+    <div className=" mt-14 mb-20">
+      <div className=" container">
+        <div className=" text-center mb-10 max-w-[600px] mx-auto">
+          <p data-aos="fade-up" className="text-sm text-primary">
+            Top selling Products For You
+          </p>
+          <h1 data-aos="fade-up" className="text-3xl font-bold">
+            Products
+          </h1>
+          <p data-aos="fade-up" className=" text-xs text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+            magnam eos dicta ullam
+          </p>
+        </div>
+        </div>
+    </div>  
   )
 }
