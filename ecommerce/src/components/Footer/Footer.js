01648b365c304bfc0fb3ac2044f36478ac416cf0
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-21 20:17:52 +0500

    Changes:

--- ecommerce/src/components/Footer/Footer.js
+++ ecommerce/src/components/Footer/Footer.js
@@ -3,6 +3,33 @@ import FooterLogo from "../../assets/logo.png"
 import Banner from "../../assets/Website/orange-pattern.jpg";
 
 export default function Footer() {
+    const BannerImg = {
+        backgroundImage: `url(${Banner})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        height: "100%",
+        width: "100%",
+      };
+
+      const Footerlinks = [
+        {
+            title: "Home",
+            link: "/#",
+        },
+        {
+            title: "About",
+            link: "/#about",
+        },
+        { 
+            title: "Contact",
+            link: "/#contact",
+        },
+        { 
+            title: "Blog",
+            link: "/#blog",
+        }
+      ]
   return (
     <div>
       <div>
