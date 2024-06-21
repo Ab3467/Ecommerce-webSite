959fa527fa9b3f5dc7dbac2452ffe4d65c34d7f9
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-21 19:13:44 +0500

    Changes:

--- ecommerce/src/components/Testimonials/Testimonial.js
+++ ecommerce/src/components/Testimonials/Testimonial.js
@@ -30,6 +30,43 @@ const TestimonialData = [
   
 
 export default function Testimonial() {
+  var settings = {
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+    pauseOnHover: true,
+    pauseOnFocus: true,
+    responsive: [
+      {
+        breakpoint: 10000,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <div className="py-10 mb-10">
       <div className=" container">
@@ -47,7 +84,11 @@ export default function Testimonial() {
           {/* Testimonial Cards */}
           <div>
             <div>
-
+           <Slider {...settings}>{
+            TestimonialData.map((data)=>(
+              <div></div>
+            ))
+            }</Slider>
             </div>
           </div>
         </div>
