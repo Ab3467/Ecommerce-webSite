9b2a38492de5d055efafacc73c5582206756f6e4
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-21 18:14:20 +0500

    Changes:

--- ecommerce/src/components/Subscribe/Subscribe.js
+++ ecommerce/src/components/Subscribe/Subscribe.js
@@ -4,21 +4,25 @@ import Banner from "../../assets/Website/orange-pattern.jpg";
 export default function Subscribe() {
   const BannerImg = {
     backgroundImage: `url(${Banner})`,
-    backgroundPosition : "center",
+    backgroundPosition: "center",
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
     height: "100%",
-    width: "100%"
+    width: "100%",
   };
 
   return (
-    <div data-aos="zoom-in"
-    className=" bg-gray-100 dark:bg-gray-800 text-white"
-    style={BannerImg}>
+    <div
+      data-aos="zoom-in"
+      className=" bg-gray-100 dark:bg-gray-800 text-white"
+      style={BannerImg}
+    >
       <div className=" container backdrop-blur-sm py-10">
         <div className="">
-          <h1 className=" text-2xl text-center sm:text-left sm:text-4xl font-semibold">Get Notified about new Products </h1>
-
+          <h1 className=" text-2xl text-center sm:text-left sm:text-4xl font-semibold">
+            Get Notified about new Products{" "}
+          </h1>
+          <input type="text" placeholder="Enter your Email" />
         </div>
       </div>
     </div>
