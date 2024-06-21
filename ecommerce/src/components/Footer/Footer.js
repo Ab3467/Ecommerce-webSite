1ec5f8f19116afd5a5da07cd9a48d7317f48c324
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-21 20:19:49 +0500

    Changes:

--- ecommerce/src/components/Footer/Footer.js
+++ ecommerce/src/components/Footer/Footer.js
@@ -1,40 +1,44 @@
-import React from 'react'
-import FooterLogo from "../../assets/logo.png"
+import React from "react";
+import FooterLogo from "../../assets/logo.png";
 import Banner from "../../assets/Website/orange-pattern.jpg";
 
 export default function Footer() {
-    const BannerImg = {
-        backgroundImage: `url(${Banner})`,
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        height: "100%",
-        width: "100%",
-      };
+  const BannerImg = {
+    backgroundImage: `url(${Banner})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    height: "100%",
+    width: "100%",
+  };
 
-      const Footerlinks = [
-        {
-            title: "Home",
-            link: "/#",
-        },
-        {
-            title: "About",
-            link: "/#about",
-        },
-        { 
-            title: "Contact",
-            link: "/#contact",
-        },
-        { 
-            title: "Blog",
-            link: "/#blog",
-        }
-      ]
+  const Footerlinks = [
+    {
+      title: "Home",
+      link: "/#",
+    },
+    {
+      title: "About",
+      link: "/#about",
+    },
+    {
+      title: "Contact",
+      link: "/#contact",
+    },
+    {
+      title: "Blog",
+      link: "/#blog",
+    },
+  ];
   return (
     <div>
-      <div>
-
+      <div className=" container">
+        <div>
+          <div className="">
+            <h1>Shopsy</h1>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
