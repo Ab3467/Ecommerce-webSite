bbf2d5cb4588672d9997c20a22d7a2398ed170d8
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-06-21 19:11:10 +0500

    Changes:

--- ecommerce/src/components/Testimonials/Testimonial.js
+++ ecommerce/src/components/Testimonials/Testimonial.js
@@ -1,4 +1,33 @@
 import React from 'react'
+import Slider from "react-slick";
+
+const TestimonialData = [
+    {
+      id: 1,
+      name: "Victor",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+      img: "https://picsum.photos/101/101",
+    },
+    {
+      id: 2,
+      name: "Satya Nadella",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+      img: "https://picsum.photos/102/102",
+    },
+    {
+      id: 3,
+      name: "Virat Kohli",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+      img: "https://picsum.photos/104/104",
+    },
+    {
+      id: 5,
+      name: "Sachin Tendulkar",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque reiciendis inventore iste ratione ex alias quis magni at optio",
+      img: "https://picsum.photos/103/103",
+    },
+  ];
+  
 
 export default function Testimonial() {
   return (
@@ -14,6 +43,13 @@ export default function Testimonial() {
           <p data-aos="fade-up" className=" text-xs text-gray-400">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit asperiores modi Sit asperiores modi
           </p>
+
+          {/* Testimonial Cards */}
+          <div>
+            <div>
+
+            </div>
+          </div>
         </div>
         </div>
     </div>  
