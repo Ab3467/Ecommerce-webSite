4328b3f0fe475c806690810ed75d28be7be87785
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-08-16 15:11:57 +0500

    Changes

--- ecommerce/src/components/Footer/Footer.js
+++ ecommerce/src/components/Footer/Footer.js
@@ -96,13 +96,13 @@ const Footer = () => {
 
             <div>
               <div className="flex items-center gap-3 mt-6">
-                <a href="#">
+                <a href="/">
                   <FaInstagram className="text-3xl" />
                 </a>
-                <a href="#">
+                <a href="/">
                   <FaFacebook className="text-3xl" />
                 </a>
-                <a href="#">
+                <a href="/">
                   <FaLinkedin className="text-3xl" />
                 </a>
               </div>
