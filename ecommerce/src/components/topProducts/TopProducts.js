78fb8141b17d449fdd03885c06d35cbf69078198
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-08-16 16:26:05 +0500

    changes

--- ecommerce/src/components/topProducts/TopProducts.js
+++ ecommerce/src/components/topProducts/TopProducts.js
@@ -6,10 +6,8 @@ import { FaStar } from "react-icons/fa6";
 import Button from "../Button";
 
 export default function TopProducts() {
-
-  function handleButton()
-  {
-    alert("App in maintenance")
+  function handleButton() {
+    alert("App in maintenance");
   }
   const ProductDetails = [
     {
@@ -52,27 +50,35 @@ export default function TopProducts() {
 
         <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-20 md:gap-5 place-items-center">
           {ProductDetails.map((data) => (
-            <div data-aos = "zoom-in" className=" rounded-2xl bg-white dark:bg-gray-800 hover:bg-black/80 dark:hover:bg-primary hover:text-white relative shadow-xl duration-300
+            <div
+              data-aos="zoom-in"
+              className=" rounded-2xl bg-white dark:bg-gray-800 hover:bg-black/80 dark:hover:bg-primary hover:text-white relative shadow-xl duration-300
             ground max-w-[300px]"
               key={data.id}
             >
               <div className=" h-[100px]">
-                <img src={data.img} alt="" className=" max-w-[140px] block mx-auto transform -translate-y-20 group-hover:scale-105 duration-300 drop-shadow-md"/>
+                <img
+                  src={data.img}
+                  alt=""
+                  className=" max-w-[140px] block mx-auto transform -translate-y-20 group-hover:scale-105 duration-300 drop-shadow-md"
+                />
               </div>
 
               <div className=" p-4 text-center">
                 <div className=" w-full flex item-center justify-center gap-1">
-                  <FaStar className=" text-yellow-500"/>
-                  <FaStar className=" text-yellow-500"/>
-                  <FaStar className=" text-yellow-500"/>
-                  <FaStar className=" text-yellow-500"/>
+                  <FaStar className=" text-yellow-500" />
+                  <FaStar className=" text-yellow-500" />
+                  <FaStar className=" text-yellow-500" />
+                  <FaStar className=" text-yellow-500" />
                 </div>
                 <h1 className=" text-xl font-bold">{data.title}</h1>
-                <p className="text-gray-600 group-hover:text-white duration-300 text-sm line-clamp-2">{data.description}</p>
+                <p className="text-gray-600 group-hover:text-white duration-300 text-sm line-clamp-2">
+                  {data.description}
+                </p>
                 <Button
-              handleButton={handleButton}
-              btnName="Order Now"
-            ></Button>
+                  handleButton={handleButton}
+                  btnName="Order Now"
+                ></Button>
               </div>
             </div>
           ))}
